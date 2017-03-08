f17ea2ea4fab3a2348260d66831bd60c8392f30b
John Hayes <john.hayes@usask.ca>
2017-03-08 14:03:45 -0600

    Fixed mpileup options in VC_Writer; changed Region_Splitter output to BED format

--- PBS_Scripts/Region_Splitter.py
+++ PBS_Scripts/Region_Splitter.py
@@ -7,17 +7,16 @@ else:
 	file = sys.argv[1]
 	save_dir = sys.argv[2]
 
-
 region_list = {'morex': [], 'H-chrom': [], 'chrom': []}
 with open(file, 'r') as f:
 	for line in f:
-		region_name = line.split('\t')[0]
-		if re.match('morex', region_name):
-			region_list['morex'].append(region_name)
-		elif re.match('\d.', region_name):
-			region_list['H-chrom'].append(region_name)
-		elif re.match('\d', region_name):
-			region_list['chrom'].append(region_name)
+		region_name = line.split('\t')
+		if re.match('morex', region_name[0]):
+			region_list['morex'].append((region_name[0], region_name[1]))
+		elif re.match('\d.', region_name[0]):
+			region_list['H-chrom'].append((region_name[0], region_name[1]))
+		elif re.match('\d', region_name[0]):
+			region_list['chrom'].append((region_name[0], region_name[1]))
 
 file_list = []
 for key in region_list:
@@ -25,7 +24,7 @@ for key in region_list:
 	file_list.append(file_name)
 	with open(file_name, 'w') as f:
 		for region in region_list[key]:
-			f.write(region + '\t1\n')
+			f.write(region[0] + '\t0\t' + str(int(region[1])-1)  + '\n')
 
 with open(join(save_dir, "region_list.txt"), 'w') as f:
 	for file in file_list:
