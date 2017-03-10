c569eb4ca8de84b764856a69cb24673ce698ee35
John Hayes <jrh154@gmail.com>
2017-03-10 13:38:40 -0600

    modified regex to actually work for Wheat

--- PBS_Scripts/Region_Splitter.py
+++ PBS_Scripts/Region_Splitter.py
@@ -1,42 +1,35 @@
 from os.path import isfile, isdir, join, basename, dirname
 import sys, re
-
+'''
 if len(sys.argv) != 3:
 	print("whoops, that doesn't work")
 else:
 	file = sys.argv[1]
 	save_dir = sys.argv[2]
+'''
+save_dir = './test_out'
+file = './test-2.fai'
 
-region_list = {'morex': [], 'H-chrom': [], 'chrom': []}
+region_list = {'AL': [], 'BL': [], 'DL': [], 'AS': [], 'BS': [], 'DS': [], 'U': [], 'B': []}
 with open(file, 'r') as f:
 	for line in f:
-<<<<<<< HEAD
-		region_name = line.split('\t')[0]
-		if re.match('.?_/dAL', region_name):
-			region_list['AL'].append(region_name)
-		elif re.match('.?_\dAS', region_name):
-			region_list['AS'].append(region_name)
-		elif re.match('.?_\dBL', region_name):
-			region_list['BL'].append(region_name)
-		elif re.match('.?_\dBS', region_name):
-			region_list['BS'].append(region_name)
-		elif re.match('.?_\dDL', region_name):
-			region_list['DL'].append(region_name)
-		elif re.match('.?_\dDS', region_name):
-			region_list['DS'].append(region_name)
-		elif re.match('.?_\dB', region_name):
-			region_list['B'].append(region_name)
-		elif re.match('_U', region_name):
-			region_list['U'].append(region_name)
-=======
 		region_name = line.split('\t')
-		if re.match('morex', region_name[0]):
-			region_list['morex'].append((region_name[0], region_name[1]))
-		elif re.match('\d.', region_name[0]):
-			region_list['H-chrom'].append((region_name[0], region_name[1]))
-		elif re.match('\d', region_name[0]):
-			region_list['chrom'].append((region_name[0], region_name[1]))
->>>>>>> 7f9520a904e65bc55a86ef1c3e351afe22c1c531
+		if re.match('.*\_\dAL', region_name[0]):
+			region_list['AL'].append((region_name[0], region_name[1]))
+		elif re.match('.*_\dAS', region_name[0]):
+			region_list['AS'].append((region_name[0], region_name[1]))
+		elif re.match('.*_\dBL', region_name[0]):
+			region_list['BL'].append((region_name[0], region_name[1]))
+		elif re.match('.*_\dBS', region_name[0]):
+			region_list['BS'].append((region_name[0], region_name[1]))
+		elif re.match('.*_\dDL', region_name[0]):
+			region_list['DL'].append((region_name[0], region_name[1]))
+		elif re.match('.*_\dDS', region_name[0]):
+			region_list['DS'].append((region_name[0], region_name[1]))
+		elif re.match('.*_\dB', region_name[0]):
+			region_list['B'].append((region_name[0], region_name[1]))
+		elif re.match('.*_U', region_name[0]):
+			region_list['U'].append((region_name[0], region_name[1]))
 
 file_list = []
 for key in region_list:
