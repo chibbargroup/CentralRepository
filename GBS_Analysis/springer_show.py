00f64d02af24d357aeba61c2013b02fb9b484d09
John Hayes <jrh154@gmail.com>
2017-03-21 12:17:09 -0600

    Added user input/arguments to springer_show

--- GBS_Analysis/springer_show.py
+++ GBS_Analysis/springer_show.py
@@ -1,10 +1,32 @@
-import sys, time
+'''
+Springer_Show v2
+J. Hayes, Written Mar 21, 2017
+
+Description:
+Reads the tab output from the GBS pipeline and assigns each sample allele to either parent A
+or parent B for linkage map analysis. The output is a csv file with each allele labelled as 
+either "a" (parent a), "b" (parent b), "h" (heterozygous), or "-" (no assignment).
+
+Note: There is no search implementation for column labels in the software, it is therefore necessary
+to relabel the columns in the .tab file to remove the file paths and extensions using Excel or a similar
+program.
+
+Usage:
+python springer_show.py <input file>
+
+User Input: While running, the program will as the user for the labels of Parent A1, Parent A2,
+Parent B1, and Parent B2; these must match exactly the column labels in the input .tab file
+
+Output:
+Outputs assigned_alleles.csv in the same folder as the as the input data file
+
+'''
+
+import sys, time, argparse
 import pandas as pd
 from collections import OrderedDict, Counter
-from os.path import isfile, isdir, join, basename, dirname
+from os.path import isfile, isdir, join, dirname
 from os import mkdir
-
-
 
 ##MODIFY THIS TO ADD RE EXPRESSION SEARCH FOR FILE NAME
 def File_Reader(file):
@@ -44,7 +66,7 @@ def Parent_Genotyper(df):
 		parent_a2 = df.ix[location, 1]
 		parent_b1 = df.ix[location, 2]
 		parent_b2 = df.ix[location, 3]
-		if parent_a1 == parent_a2 and parent_b1 == parent_b2:
+		if parent_a1 == parent_a2 and parent_b1 == parent_b2 and parent_a1 != './.' and parent_b1 != './.':
 			if parent_a1 != parent_b1:
 				genotype_dict[location] = {"A": parent_a1, "B": parent_b1}
 			else:
@@ -88,10 +110,49 @@ def Check_Heterozygous(sample_allele, parent_alleles):
 	else:
 		return "-"
 
-file = './test.tab'
-parent_dict = {'A': ["WI", "WII"], 'B': ["LJIIa", "LJIIb"]}
-df = File_Reader(file)
-parent_df = Parent_Parse(df, parent_dict)
-parent_genos = Parent_Genotyper(parent_df)
-final_df = Sample_Genotyper(df, parent_genos)
-final_df.to_csv("test_out.csv")
+def Parent_Label_Input():
+	correct = False
+	yes = ['y', 'yes', 'yup']
+	no = ['n', 'no', 'nope']
+	while not correct:
+		parent_a1 = input("Please input the sample label for Parent A1: ")
+		parent_a2 = input("Please input the sample label for Parent A2: ")
+		parent_b1 = input("Please input the sample label for Parent B1: ")
+		parent_b2 = input("Please input the sample label for Parent B2: ")
+		parent_dict = {'A': [parent_a1, parent_a2], 'B': [parent_b1, parent_b2]}
+		print("Here are the parents you input: ")
+		print(parent_dict)
+		correct = Yes_No_Response("Is this correct? [y/n]: ")
+	return parent_dict
+
+def Yes_No_Response(question_string):
+	positive_response = ['yes', 'y', 'yup', 'yea', 'yeah']
+	negative_response = ['no', 'n', 'nope', 'nah', 'nay']
+	valid_response = False
+	while not valid_response:
+		response = input(question_string)
+		if response.lower() in positive_response:
+			valid_response = True
+			return True
+		elif response.lower() in negative_response:
+			valid_response = True
+			return False
+		else:
+			print("I'm sorry Dave, I can't do that. Please input a valid response.")
+
+def __Main__():
+	if len(sys.argv) < 2:
+		print("Sorry, you need add the input file as an argument when calling me...")
+	elif len(sys.argv) > 2:
+		print("Sorry, there are too many arguments, please only give me the input file to analyze")
+	else:
+		file = sys.argv[1]
+		file_dir = dirname(file)
+		parent_dict = Parent_Label_Input()
+		df = File_Reader(file)
+		parent_df = Parent_Parse(df, parent_dict)
+		parent_genos = Parent_Genotyper(parent_df)
+		final_df = Sample_Genotyper(df, parent_genos)
+		final_df.to_csv(join(file_dir, "assigned_alleles.csv"))
+
+__Main__()
